621ead124990897470697cdb817d21a7f59df953
davidessss <dvdgsxr@gmail.com>
2015-03-13 14:37:58 +0100

    Update checkip.py

--- checkip.py
+++ checkip.py
@@ -1,38 +1,46 @@
 #!/bin/python
+import os
 import subprocess
 import sys
 from email_utils import EmailConnection, Email
-import sqlite3
-conn = sqlite3.connect('/var/tmp/casa.sqlite')
 
+name = 'Your name'
+email = 'Your e-mail'
+password = 'Your password'
+mail_server = 'Your mail server'
+to_email = 'Destination email'
+to_name = 'Name of destination'
+subject = 'Change IP'
+message = 'Your new IP is:'
 
-sql = "SELECT Ip, Id FROM `mio_ip` ORDER BY Id DESC"
+fileaddress = os.path.dirname(os.path.abspath(__file__))+'/address'
 
-c = conn.cursor()
-
-c.execute(sql)
-
-old_ip = c.fetchone()[0]
-
+if os.path.isfile(fileaddress):
+	f = open(fileaddress, 'r')
+	old_ip = f.readline()
+else:
+	old_ip = 'new'
 
 new_ip = subprocess.check_output(['curl ifconfig.me'], shell=True)
+
 
 print "Your old IP " + old_ip
 print "Your new IP " + new_ip
 
-if new_ip != old_ip:
-	print 'esegui query cambio IP'
-	c.execute("INSERT INTO  `mio_ip` (`id` ,`ip` ,`data`)VALUES ((SELECT max(id) FROM mio_ip)+1 ,  '%s', date('now'));" % (new_ip))
-	conn.commit()
-        conn.close
-        print 'Connecting to server...'
-	server = EmailConnection()
+if old_ip != new_ip:
+	print 'change the address in the file'
+	f = open(fileaddress, "w")
+	f.write(new_ip)
+	print 'Connecting to server...'
+	server = EmailConnection(mail_server, email, password)
 	print 'Preparing the email...'
-	email = Email(from_='"Raspberry" <yourmail@gmail.com>', to='"yourname" <yourmail@gmail.com>',subject='Change IP', message='Your new IP address ' + new_ip)
+	email = Email(from_='"%s" <%s>' % (name, email), #you can pass only email
+              to='"%s" <%s>' % (to_name, to_email), #you can pass only email
+              subject=subject, message='%s %s' % (message,new_ip))
 	print 'Sending...'
 	server.send(email)
 	print 'Disconnecting...'
 	server.close()
 	print 'Done!'
-else:
-	print 'IP Uguale'	
+else
+	print "Same IP" + new_ip
